42e81abed72f010ec09d74c806a35e943d3357da
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-04-04 09:02:16 -0300

    Adicionando funções
    função: inserirProduto e listarUmProduto

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -20,8 +20,37 @@ function listarProdutos(PDO $conexao):array {
     }
 }
 
-function inserirProduto( PDO $conexao, string $nome, float $preco, int $quantidade,
-                    int $fabricantes, string $descricao ):void {
-                                
+function inserirProduto( 
+    PDO $conexao, string $valorNome, float $valorPreco,
+    int $valorQuantidade, int $idFabricante, string $valorDescricao  
+    ):void {
+
+        $sql = "INSERT INTO produtos(nome, descricao, preco, quantidade, fabricante_id)
+                VALUES(:nome, :descricao, :preco, :quantidade, :fabricante_id)";
+
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":nome", $valorNome, PDO::PARAM_STR);
+        $consulta->bindValue(":descricao", $valorDescricao, PDO::PARAM_STR);
+        $consulta->bindValue(":preco", $valorPreco, PDO::PARAM_STR);
+        $consulta->bindValue(":quantidade", $valorQuantidade, PDO::PARAM_INT);
+        $consulta->bindValue(":fabricante_id", $idFabricante, PDO::PARAM_INT);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao inserir produto: ".$erro->getMessage());
+    }              
 }
-?>
+
+function listarUmProduto(PDO $conexao, int $idProduto):array {
+    $sql = "SELECT * FROM produtos WHERE id = :id";
+ 
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":id", $idProduto, PDO::PARAM_INT);
+        $consulta->execute();
+        return $consulta->fetch(PDO::FETCH_ASSOC);
+    }
+        catch (Exception $erro) {
+            die("Erro ao inserir produto: ".$erro->getMessage());
+    }   
+}
